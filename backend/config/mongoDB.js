75705190fdc022b22377dd2fe46411cc179cce0e
Diptoprovo Mondal <118748808+Diptoprovo@users.noreply.github.com>
2025-01-25 23:59:33 +0530

    Update mongoDB.js

--- backend/config/mongoDB.js
+++ backend/config/mongoDB.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
 
     await mongoose.connect(`mongodb+srv://devwrik1306:handabhonda@cluster0.agkac.mongodb.net/chat-app`)
     //await mongoose.connect(`${process.env.MONGODB_URI}/chat-app`);
-    console.log(process.env.MONGODB_URI);
+    //console.log(process.env.MONGODB_URI);
 }
 
 export default connectDB;
