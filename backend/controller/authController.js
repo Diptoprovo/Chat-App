d16b73f75b96e24d079db5b75362da096cfc416d
Diptoprovo Mondal <118748808+Diptoprovo@users.noreply.github.com>
2025-01-26 00:15:58 +0530

    Update authController.js

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -23,10 +23,10 @@ export const register = async (req, res) => {
         const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '7d' });
 
         res.cookie('token', token, {
-            httpOnly: true,
+            //httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
-            // sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
-            sameSite: 'strict',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+            //sameSite: 'strict',
             maxAge: 7 * 24 * 60 * 60 * 1000
         });
 
@@ -57,10 +57,10 @@ export const login = async (req, res) => {
         const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '7d' });
 
         res.cookie('token', token, {
-            httpOnly: true,
+            //httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
-            // sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
-            sameSite: 'strict',
+             sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+            //sameSite: 'strict',
             maxAge: 7 * 24 * 60 * 60 * 1000
         });
 
@@ -73,7 +73,7 @@ export const login = async (req, res) => {
 export const logout = async (req, res) => {
     try {
         res.clearCookie('token', {
-            httpOnly: true,
+            //httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
             sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict'
         })
