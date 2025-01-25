c417680982c2e84c1a09992bdab0078ebc92c5c1
Diptoprovo Mondal <118748808+Diptoprovo@users.noreply.github.com>
2025-01-26 00:30:42 +0530

    Update authController.js

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -23,7 +23,7 @@ export const register = async (req, res) => {
         const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '7d' });
 
         res.cookie('token', token, {
-            //httpOnly: true,
+            httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
             sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
             //sameSite: 'strict',
@@ -57,7 +57,7 @@ export const login = async (req, res) => {
         const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '7d' });
 
         res.cookie('token', token, {
-            //httpOnly: true,
+            httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
              sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
             //sameSite: 'strict',
@@ -73,7 +73,7 @@ export const login = async (req, res) => {
 export const logout = async (req, res) => {
     try {
         res.clearCookie('token', {
-            //httpOnly: true,
+            httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
             sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict'
         })
